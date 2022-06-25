02712afd4c2e3830cb409e9249bff4787b3fadcb
koirul92 <koirulanwar98@gmail.com>
2022-06-25 14:28:35 +0700

    fix id drivers

--- app/controllers/drivers.controller.js
+++ app/controllers/drivers.controller.js
@@ -4,20 +4,11 @@ const accService = require('../services/accdriver.service')
 exports.list = async (req, res) => {
     try {
         const response = await driverService.list()
-        const {data: datas} = response
-        const driver = datas.map(item => ({
-            full_name: item.full_name,
-            email: item.email,
-            password: item.password,
-            phone_number: item.password,
-            address: item.address,
-            no_ktp: item.no_ktp,
-            file: `http://localhost:3001/public/uploads/${item.file}`,
-        }))
+        const {data} = response
 
         res.json({
             status: "OK",
-            data: driver
+            data
         })
     } catch(error) {
         res.status(400).json({
